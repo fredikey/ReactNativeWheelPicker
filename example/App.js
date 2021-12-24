dfd66b752425b5acf43c6863528c68a4e2faec1a
Victor Kozodaev <viktor7778123@gmail.com>
2021-12-25 00:18:39 +0300

    Remove: DatePicker & TimePicker

--- example/App.js
+++ example/App.js
@@ -15,9 +15,8 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-import moment from 'moment'
 
-import { WheelPicker, TimePicker, DatePicker } from 'react-native-wheel-picker-android'
+import { WheelPicker } from 'react-native-wheel-picker-android'
 
 const weekdays = [
   'sunday',
@@ -30,8 +29,6 @@ const weekdays = [
 
 const App: () => React$Node = () => {
   const [selectedItem, setSelectedItem] = useState(0)
-  const [time, setTime] = useState(new Date())
-  const [date, setDate] = useState(new Date())
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -40,16 +37,6 @@ const App: () => React$Node = () => {
           contentInsetAdjustmentBehavior="automatic">
           <Text>{weekdays[selectedItem]}</Text>
           <WheelPicker onItemSelected={(index) => setSelectedItem(index)} data={weekdays}/>
-          <Text>{moment(time).format('hh:mm a')}</Text>
-          <TimePicker 
-            initDate={new Date(moment().add(1, 'hour'))} 
-            onTimeSelected={(time) => {
-              setTime(time)
-              console.log('setting time', time)
-            }
-          }/>
-          <Text>{moment(date).format('DD.MM.YY hh:mm a')}</Text>
-          <DatePicker mode={'date'} onDateSelected={date => setDate(date)}/>
         </ScrollView>
       </SafeAreaView>
     </>
--- index.tsx
+++ index.tsx
@@ -1,5 +1,3 @@
 import WheelPicker from './src/WheelPicker'
-import TimePicker from './src/TimePicker'
-import DatePicker from './src/DatePicker'
 
-export { WheelPicker, TimePicker, DatePicker }
+export { WheelPicker }
